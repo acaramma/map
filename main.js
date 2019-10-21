2a67800f7db86615ae4741ef48434cebd78e4789
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-21 15:41:04 +0200

    aggiunti marker con popup

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 navigator.geolocation.getCurrentPosition(function(position) {
 	var lat = position.coords.latitude;
     var lng = position.coords.longitude;
-    mapboxgl.accessToken = config.mapboxgl.accessToken;
+    mapboxgl.accessToken = config;
 	var map = new mapboxgl.Map({
 		container: 'map',
 		style: 'mapbox://styles/mapbox/streets-v11',
@@ -22,23 +22,71 @@ navigator.geolocation.getCurrentPosition(function(position) {
 		trackUserLocation: true
 	}));
 
-	
+	map.on('load', function () {
+	map.addLayer({
+		"id": "places",
+		"type": "symbol",
+		"source": {
+			"type": "geojson",
+			"data": {
+				"type": "FeatureCollection",
+				"features": [{
+					"type": "Feature",
+					"properties": {
+						"description": "<strong>Make it Mount Pleasant</strong><p><a href=\"http://www.mtpleasantdc.com/makeitmtpleasant\" target=\"_blank\" title=\"Opens in a new window\">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
+						"icon": "theatre"
+					},
+					"geometry": {
+						"type": "Point",
+						"coordinates": [9.147715, 45.397865]
+					}
+				}, {
+					"type": "Feature",
+					"properties": {
+						"description": "<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a <a href=\"http://madmens5finale.eventbrite.com/\" target=\"_blank\" title=\"Opens in a new window\">Mad Men Season Five Finale Watch Party</a>, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>",
+						"icon": "theatre"
+					},
+					"geometry": {
+						"type": "Point",
+						"coordinates": [9.148171, 45.399315]
+					}
+				}, {
+					"type": "Feature",
+					"properties": {
+						"description": "<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a <a href=\"http://tallulaeatbar.ticketleap.com/2012beachblanket/\" target=\"_blank\" title=\"Opens in a new window\">Big Backyard Beach Bash and Wine Fest</a> on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>",
+						"icon": "bar"
+					},
+					"geometry": {
+						"type": "Point",
+						"coordinates": [9.144366, 45.398647]
+					}
+				}]
+			}
+		},
+		"layout": {
+			"icon-image": "{icon}-15",
+			"icon-allow-overlap": true
+		}
+	});
 
-	//marker per p.v
-	var teatro = [9.147715, 45.397865];	 
-	var marker = new mapboxgl.Marker()
-	.setLngLat(teatro)
-	.addTo(map);
+	map.on('click', 'places', function (e) {
+		var coordinates = e.features[0].geometry.coordinates.slice();
+		var description = e.features[0].properties.description;
+		while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+			coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+		}	 
+		new mapboxgl.Popup()
+		.setLngLat(coordinates)
+		.setHTML(description)
+		.addTo(map);
+	});
 
-	//marker per p.v
-	var supermercato = [9.148171, 45.399315];	 
-	var marker = new mapboxgl.Marker()
-	.setLngLat(supermercato)
-	.addTo(map);
+	map.on('mouseenter', 'places', function () {
+		map.getCanvas().style.cursor = 'pointer';
+	});
 
-	//marker per p.v
-	var parco = [9.144366, 45.398647];
-	var marker = new mapboxgl.Marker()
-	.setLngLat(parco)
-	.addTo(map);
+	map.on('mouseleave', 'places', function () {
+		map.getCanvas().style.cursor = '';
+		});
+	});
 });
