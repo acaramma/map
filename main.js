b912deff6f62ff7768f2054e89bb9bed223bb5af
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-21 14:41:02 +0200

    barra ricerca

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-/*navigator.geolocation.getCurrentPosition(function(position) {
+navigator.geolocation.getCurrentPosition(function(position) {
 	var lat = position.coords.latitude;
     var lng = position.coords.longitude;
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWNhcmFtbWEiLCJhIjoiY2sxdzA1ODgxMDkxbzNucDBkNTlnbjlqdSJ9.25isL_da_uHyJGiG2Mdr6A';
+    mapboxgl.accessToken = config.mapboxgl.accessToken;
 	var map = new mapboxgl.Map({
 		container: 'map',
 		style: 'mapbox://styles/mapbox/streets-v11',
 		center: [lng, lat],
 		zoom: 15
-	});	
+	});
 
-	// bottone per ritornare alla pos dell'utente
+	map.addControl(new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		mapboxgl: mapboxgl
+	}));	
+
+	// bottone per localizzare utente
 	map.addControl(new mapboxgl.GeolocateControl({
-	positionOptions: {
-		enableHighAccuracy: true
+		positionOptions: {
+			enableHighAccuracy: true
 		},
 		trackUserLocation: true
 	}));
+
+	
 
 	//marker per p.v
 	var teatro = [9.147715, 45.397865];	 
@@ -30,49 +37,8 @@
 	.addTo(map);
 
 	//marker per p.v
-	var parco = [9.144366, 45.398647];	 
+	var parco = [9.144366, 45.398647];
 	var marker = new mapboxgl.Marker()
 	.setLngLat(parco)
 	.addTo(map);
-});*/
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWNhcmFtbWEiLCJhIjoiY2sxdzA1ODgxMDkxbzNucDBkNTlnbjlqdSJ9.25isL_da_uHyJGiG2Mdr6A';
-var map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v11',
-});	
-
-// bottone per ritornare alla pos dell'utente
-map.addControl(new mapboxgl.GeolocateControl({
-positionOptions: {
-	enableHighAccuracy: true
-	},
-	trackUserLocation: true
-}));
-
-navigator.geolocation.getCurrentPosition(position => {
-  const userCoordinates = [position.coords.longitude, position.coords.latitude];
-  map.addSource("user-coordinates", {
-    type: "geojson",
-    data: {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: userCoordinates
-      }
-    }
-  });
-  map.addLayer({
-    id: "user-coordinates",
-    source: "user-coordinates",
-    type: "circle"
-  });
-  map.flyTo({
-    center: userCoordinates,
-    zoom: 15
-  });
-  this.setState({
-    resolvingLocation: false,
-    userCoordinates
-  });
 });
