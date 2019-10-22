af1fdb8ae48fd902135e9d9dcf270dcb20b23c64
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-22 11:52:19 +0200

    aggiunti marker corretti

--- main.js
+++ main.js
@@ -23,64 +23,27 @@ navigator.geolocation.getCurrentPosition(function(position) {
 	}));
 
 	map.on('load', function () {
-		map.addLayer({
-			"id": "places",
-			"type": "symbol",
-			"source": {
-				"type": "geojson",
-				"data": {
-					"type": "FeatureCollection",
-					"features": [{
-						"type": "Feature",
-						"properties": {
-							"description": "<strong>One More Light</strong><p>Who cares if one more light goes out? In a sky of a million stars, It flickers, flickers, Who cares when someone's time runs out?, If a moment is all we are, We're quicker, quicker, Who cares if one more light goes out? Well I do.</p>",
-							"icon": "marker"
-						},
-						"geometry": {
-							"type": "Point",
-							"coordinates": [9.147715, 45.397865]
-						}
-					}, {
-						"type": "Feature",
-						"properties": {
-							"description": "<strong>Sharp Edges</strong><p>Sharp edges have consequences, I guess that I had to find out for myself, Sharp edges have consequences, Now every scar is a story I can tell.</p>",
-							"icon": "marker"
-						},
-						"geometry": {
-							"type": "Point",
-							"coordinates": [9.148171, 45.399315]
-						}
-					}, {
-						"type": "Feature",
-						"properties": {
-							"description": "<strong>Battle Symphony</strong><p>I hear my battle symphony, All the world in front of me, If my armor breaks, I'll fuse it back together, Battle symphony, Please just don't give up on me, And my eyes are wide awake.</p>",
-							"icon": "marker"
-						},
-						"geometry": {
-							"type": "Point",
-							"coordinates": [9.144366, 45.398647]
-						}
-					}]
-				}
-			},
-			"layout": {
-				"icon-image": "{icon}-15",
-				"icon-size": 2,
-				"icon-allow-overlap": true
-			}
-		});
+		//marker
+		new mapboxgl.Marker()
+		.setLngLat([9.144366, 45.398647])
+		.setPopup(new mapboxgl.Popup({ offset: 25 })
+    		.setHTML("<strong>Battle Symphony</strong><p>I hear my battle symphony, All the world in front of me, If my armor breaks, I\'ll fuse it back together, Battle symphony, Please just don\'t give up on me, And my eyes are wide awake.</p>"))
+		.addTo(map);
 
-		map.on('click', 'places', function (e) {
-			var coordinates = e.features[0].geometry.coordinates.slice();
-			var description = e.features[0].properties.description;
-			while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-				coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-			}	 
-			new mapboxgl.Popup()
-			.setLngLat(coordinates)
-			.setHTML(description)
-			.addTo(map);
-		});
+
+		new mapboxgl.Marker()
+		.setLngLat([9.148171, 45.399315])
+		.setPopup(new mapboxgl.Popup({ offset: 25 })
+    		.setHTML("<strong>Sharp Edges</strong><p>Sharp edges have consequences, I guess that I had to find out for myself, Sharp edges have consequences, Now every scar is a story I can tell.</p>"))
+		.addTo(map);
+
+
+
+		new mapboxgl.Marker()
+		.setLngLat([9.147715, 45.397865])
+		.setPopup(new mapboxgl.Popup({ offset: 25 })
+    		.setHTML("<strong>One More Light</strong><p>Who cares if one more light goes out? In a sky of a million stars, It flickers, flickers, Who cares when someone's time runs out?, If a moment is all we are, We're quicker, quicker, Who cares if one more light goes out? Well I do.</p>"))
+		.addTo(map);
 
 		map.on('mouseenter', 'places', function () {
 			map.getCanvas().style.cursor = 'pointer';
@@ -90,6 +53,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
 			map.getCanvas().style.cursor = '';
 		});
 
+		//cerchio 
 		map.addSource("source_circle", {
         	"type": "geojson",
         	"data": {
@@ -112,7 +76,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
           		"circle-radius": {
             		stops: [
               			[3, 1],
-              			[15, 500]
+              			[15, 250]
             		],
             		base: 2
           		},
