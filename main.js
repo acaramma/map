f86300eae41b26cd33587b51394b7b84de84745c
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-22 12:19:48 +0200

    fixed

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
 		trackUserLocation: true
 	}));
 
-	map.on('load', function () {
+	map.once('load', function () {
 		//marker
 		new mapboxgl.Marker()
 		.setLngLat([9.144366, 45.398647])
@@ -30,14 +30,11 @@ navigator.geolocation.getCurrentPosition(function(position) {
     		.setHTML("<strong>Battle Symphony</strong><p>I hear my battle symphony, All the world in front of me, If my armor breaks, I\'ll fuse it back together, Battle symphony, Please just don\'t give up on me, And my eyes are wide awake.</p>"))
 		.addTo(map);
 
-
 		new mapboxgl.Marker()
 		.setLngLat([9.148171, 45.399315])
 		.setPopup(new mapboxgl.Popup({ offset: 25 })
     		.setHTML("<strong>Sharp Edges</strong><p>Sharp edges have consequences, I guess that I had to find out for myself, Sharp edges have consequences, Now every scar is a story I can tell.</p>"))
 		.addTo(map);
-
-
 
 		new mapboxgl.Marker()
 		.setLngLat([9.147715, 45.397865])
@@ -45,15 +42,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
     		.setHTML("<strong>One More Light</strong><p>Who cares if one more light goes out? In a sky of a million stars, It flickers, flickers, Who cares when someone's time runs out?, If a moment is all we are, We're quicker, quicker, Who cares if one more light goes out? Well I do.</p>"))
 		.addTo(map);
 
-		map.on('mouseenter', 'places', function () {
-			map.getCanvas().style.cursor = 'pointer';
-		});
-
-		map.on('mouseleave', 'places', function () {
-			map.getCanvas().style.cursor = '';
-		});
-
-		//cerchio 
+		//cerchio
 		map.addSource("source_circle", {
         	"type": "geojson",
         	"data": {
