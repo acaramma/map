fd721cab1c51bde50e542762aef8ea0c6088047a
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-25 14:53:07 +0200

    fixed

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ navigator.geolocation.getCurrentPosition(function(position) {
 		zoom: 14
 	});
 
+	//inserimento luogo
 	map.addControl(new MapboxGeocoder({
 		accessToken: mapboxgl.accessToken,
 		mapboxgl: mapboxgl
-	}));	
+	}));
 
 	// bottone per localizzare utente
 	map.addControl(new mapboxgl.GeolocateControl({
@@ -22,8 +23,8 @@ navigator.geolocation.getCurrentPosition(function(position) {
 		trackUserLocation: true
 	}));
 
-	map.once('load', function () {
-		//marker
+	map.on('load', function () {
+		//markers
 		var descrizione = JSON.stringify(descrizioni.descrizione1);
 		new mapboxgl.Marker()
 		.setLngLat([9.144366, 45.398647])
@@ -55,16 +56,16 @@ navigator.geolocation.getCurrentPosition(function(position) {
 		//cerchio
 		map.addSource("source_circle", {
 			"type": "geojson",
-	        	"data": {	        		
-	          		"type": "FeatureCollection",
-	          		"features": [{
-	            		"type": "Feature",
-	            		"geometry": {
-	              			"type": "Point",
-	              			"coordinates": [lng, lat]
-	            		}
-	          		}]
-	        	}
+        	"data": {	        		
+          		"type": "FeatureCollection",
+          		"features": [{
+            		"type": "Feature",
+            		"geometry": {
+              			"type": "Point",
+              			"coordinates": [lng, lat]
+            		}
+          		}]
+        	}
     	});
 
       	map.addLayer({        		
